cf751a8c1f8b829414296b5659508fe04e906f13
Coding Bob <85940918+BobRoblox2020@users.noreply.github.com>
2022-03-24 08:33:50 +0700

    Update manager.js

--- events/manager.js
+++ events/manager.js
@@ -1,5 +1,10 @@
 const { Manager } = require("erela.js");
-const { MessageEmbed } = require('discord.js')
+const Spotify = require('erela.js-spotify')
+const { MessageEmbed } = require('discord.js');
+const config = require('../config.json')
+
+const clientID = "79ab9e85ef4c4bc28ae19cbe83cc32bc"
+const clientSecret = "03a23e074ef34787b858853746e5337a"
 
 module.exports = function(client) {
     return new Manager({
@@ -9,7 +14,12 @@ module.exports = function(client) {
             port: 80,
             password: "youshallnotpass",
             secure: false,
+            retryAmount: Infinity,
+            retryDelay: 3000,
         },
+      ],
+      plugins: [
+        new Spotify({ clientID, clientSecret })
       ],
       send(id, payload) {
         const guild = client.guilds.cache.get(id);
@@ -21,11 +31,18 @@ module.exports = function(client) {
         console.log(`[NODES]: Node ${node.options.identifier} connected`)
       )
       .on("nodeError", (node, error) => 
-        console.log(`Node ${node.options.identifier} had an error: ${error.message}`)
+        console.log(`[NODES]: Node ${node.options.identifier} had an error: ${error.message}`)
+      )
+      .on("nodeReconnect", node => 
+        console.log(`[NODES]: Node ${node.options.identifier} reconnecting`)
+      )
+      .on("nodeDisconnect", node => 
+        console.log(`[NODES]: Node ${node.options.identifier} disconnected`)
       )
       .on("trackStart", (player, track) => {
         client.channels.cache
           .get(player.textChannel)
+          .send({ embeds: [new MessageEmbed().setTitle("NOW PLAYING").setDescription(`[${track.title}](${track.uri})`).setColor(config.color)]})
       })
       .on("trackEnd", (player) => {
         client.channels.cache
