afbbdf289834f4a5b8173b6b5367c652edd1ed0e
Coding Bob <85940918+BobRoblox2020@users.noreply.github.com>
2022-03-24 08:36:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const discord = require("discord.js");
 const {token} = require("./config.json")
 const {Intents} = require("discord.js");
+const manager = require('./events/manager')
+
 
 const client = new discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
-client.manager = require('./events/manager')(client);
-client.on("raw", (d) => client.manager.updateVoiceState(d));
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
 client.cooldowns = new discord.Collection();
+client.manager = require('./events/manager')(client)
 client.slashcommands = new discord.Collection();
 
 ["Handler"].forEach(handler => {
@@ -22,5 +23,7 @@ process.on("unhandledRejection", (reason, promise) => {
       console.error(reason);
   }
 });
+
+client.on("raw", (d) => client.manager.updateVoiceState(d));
 
 client.login(token)
